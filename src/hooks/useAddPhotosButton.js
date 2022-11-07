75691e951ede16487b77ade95729bcc4ea35f780
Viktor <viktor_hordubey@yahoo.com>
2022-11-07 22:13:41 +0200

    clean up

--- src/hooks/useAddPhotosButton.js
+++ src/hooks/useAddPhotosButton.js
@@ -4,48 +4,50 @@ import { Platform } from 'react-native';
 import { palette } from '../Theme';
 import useGalleryPermissionHandler from './useGalleryPermissionHandler';
 
-export const useAddPhotosButton = ( onPhotosSelected, maxPhotosAllowed, selectedPhotos ) => {
-	const _getBase64 = async ( images ) => {
-		await Promise.all( images.map( async ( image ) => {
-			const base64 = await RNSF.readFile( image.path, 'base64' );
-			image.base64 = base64;
-		} ) );
-	};
+export const useAddPhotosButton = (onPhotosSelected, maxPhotosAllowed, selectedPhotos) => {
+  const _getBase64 = async (images) => {
+    await Promise.all(
+      images.map(async (image) => {
+        const base64 = await RNSF.readFile(image.path, 'base64');
+        image.base64 = base64;
+      })
+    );
+  };
 
-	const mediaPathForAndroid = ( media ) => `file://${media.realPath}`;
+  const mediaPathForAndroid = (media) => `file://${media.realPath}`;
 
-	const mediaPathForIos = ( media ) => media.path.replace( 'file://', '' );
+  const mediaPathForIos = (media) => media.path.replace('file://', '');
 
-	const _onImagesSelected = ( images ) => {
-		const _photos = images.map( ( image ) => ( {
-			...image,
-			uri: Platform.select( {
-				android: mediaPathForAndroid( image ),
-				ios: mediaPathForIos( image )
-			} ),
-			base64: image.base64
-		} ) );
-		onPhotosSelected( _photos );
-	};
+  const _onImagesSelected = (images) => {
+    const _photos = images.map((image) => ({
+      ...image,
+      uri: Platform.select({
+        android: mediaPathForAndroid(image),
+        ios: mediaPathForIos(image),
+      }),
+      base64: image.base64,
+    }));
+    onPhotosSelected(_photos);
+  };
 
-	const _openImagePicker = async () => {
-		try {
-			const multipleImagePickerOptions = {
-				selectedColor: palette.primary.default,
-				maxSelectedAssets: maxPhotosAllowed,
-				mediaType: 'image',
-				selectedAssets: selectedPhotos,
-				usedCameraButton: false,
-				maximumMessageTitle: 'Limit Reached'
-			};
+  const _openImagePicker = async () => {
+    try {
+      const multipleImagePickerOptions = {
+        selectedColor: palette.primary.default,
+        maxSelectedAssets: maxPhotosAllowed,
+        mediaType: 'image',
+        selectedAssets: selectedPhotos,
+        usedCameraButton: false,
+        maximumMessageTitle: 'Limit Reached',
+      };
 
-			let images = await MultipleImagePicker.openPicker( multipleImagePickerOptions );
-			await _getBase64( images );
-			_onImagesSelected( images );
-		} catch { }
-	};
+      let images = await MultipleImagePicker.openPicker(multipleImagePickerOptions);
+      await _getBase64(images);
+      _onImagesSelected(images);
+    } catch {}
+  };
 
-	const handleGalleryPermission = useGalleryPermissionHandler( _openImagePicker );
+  const handleGalleryPermission = useGalleryPermissionHandler(_openImagePicker);
 
-	return handleGalleryPermission;
+  return handleGalleryPermission;
 };
